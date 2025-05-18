3e53e891ed5198d2f87b96e0befcafc07ae314d5
DominikZuercherQC <dominik.zuercher@quantco.com>
2025-05-18 17:22:27 +0200

    wip

--- tabulardelta/comparators/polars_comparator.py
+++ tabulardelta/comparators/polars_comparator.py
@@ -55,8 +55,8 @@ class PolarsComparator:
         return compare_polars(
             old,
             new,
-            old.dtypes.astype("string").to_dict(),
-            new.dtypes.astype("string").to_dict(),
+            {col: str(dtype) for col, dtype in zip(old.columns, old.dtypes)},
+            {col: str(dtype) for col, dtype in zip(new.columns, new.dtypes)},
             self.join_columns,
             self.name,
             self.float_rtol,
@@ -64,23 +64,45 @@ class PolarsComparator:
             self.check_row_order,
         )
 
+
+def _join(
+    old: pl.DataFrame, new: pl.DataFrame, join_cols: list[str], suffixes
+) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]:
+    """Join two DataFrames on join_cols, failing for non-unique join columns.
+
+    Use outer join to get added and removed rows. Use inner join to not change data
+    types, since outer join adds Nones.
+    """
+    if old.select(join_cols).dtypes != old.select(join_cols).dtypes:
+        raise Exception("Datatypes of join columns changed. Cannot join dataframes for comparison."
+                        f" Old dtypes {old.select(join_cols).dtypes}, "
+                        f"New dtypes {old.select(join_cols).dtypes}")
+    cols_left = [f"{col}_left" for col in old.columns if col not in join_cols]
+    cols_right = [f"{col}_right"  for col in new.columns if col not in join_cols]
+    outer = old.join(new, on=join_cols, how="outer", suffix=suffixes[1])
+    # Added rows are in new but not in old
+    added_rows = old.filter(pl.all(pl.col(f"{col}_new").is_not_null() for col in join_cols))
+    # Removed rows are in old but not in new
+    removed_rows = old.filter(pl.all(pl.col(f"{col}_old").is_not_null() for col in join_cols))
+    # Joined rows are in both
+    joined = old.filter(pl.all(pl.col(f"{col}_old").is_not_null() for col in join_cols))
+    return added_rows, removed_rows, joined
+
 def compare_polars(
     old: pl.DataFrame,
     new: pl.DataFrame,
     old_dtypes: dict[str, str],
     new_dtypes: dict[str, str],
-    join_columns: list[str] | None = None,
+    join_columns: list[str],
     name: str = "",
     float_rtol: float = 1.0e-5,
     float_atol: float = 0,
     check_row_order: bool = True,
 ) -> TabularDelta:
-    """Compare pandas Dataframes.
-
-    If data was de-serialized imperfectly, the original dtypes can be specified.
+    """Compare polars Dataframes.
 
     Arguments:
-        old :class:`pd.DataFrame`:
+        old :class:`pl.DataFrame`:
             The old table (first table to compare).
         new :class:`pd.DataFrame`:
             The new table (second table to compare).
@@ -88,8 +110,8 @@ def compare_polars(
             The dtypes of the old table (name -> type).
         new_dtypes :class:`dict[str, str]`:
             The dtypes of the new table (name -> type).
-        join_columns :class:`list[str]` | :code:`None`:
-            Columns to join on, uses index if unspecified.
+        join_columns :class:`list[str]`:
+            Columns to join on
         name :class:`str`:
             Name of the comparison/tables.
         float_rtol :class:`float`:
@@ -102,3 +124,30 @@ def compare_polars(
     Returns :class:`TabularDelta`:
         Metadata and results of the comparison.
     """
+
+    warnings = []
+    info: list[str] = []
+    if check_row_order:
+        old = old.with_columns(_old_row_number=range(old.shape[0]))
+        new = new.with_columns(_new_row_number=range(new.shape[0]))
+
+    # 1. Join dataframes on join_columns
+    suffixes = ("_old", "_new")
+    try:
+        added_rows, removed_rows, joined = _join(old, new, join_columns, suffixes)
+    except KeyError as e:  # Probably non-unique join columns
+        warnings.append(
+            f"Error when joining on columns {join_columns}: {e}\n"
+            f"Using all common columns as join columns instead.\n"
+            f"MODIFIED ROWS will be LISTED AS ADDED AND REMOVED"
+        )
+        common = [col for col in old.columns if col in new.columns]
+        join_columns = [col for col in common if old_dtypes[col] == new_dtypes[col]]
+        added_rows, removed_rows, joined = _join(old, new, join_columns, suffixes)
+    # if check_row_order:
+    #     joined = joined.sort_values("_old_row_number")
+    #     if not joined["_new_row_number"].is_monotonic_increasing:
+    #         info.append("Row Order Changed!")
+    #     joined.drop(columns=["_old_row_number", "_new_row_number"], inplace=True)
+    #     added_rows.drop(columns=["_new_row_number"], inplace=True)
+    #     removed_rows.drop(columns=["_old_row_number"], inplace=True)
